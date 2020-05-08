f297f2591f84d55e71ba2845bc1bc863b9e2abe9
sebam <martinsebas8@gmail.com>
2020-05-08 18:03:08 +0200

    mas cambios a algolia service

--- src/app/services/algolia/algolia.service.ts
+++ src/app/services/algolia/algolia.service.ts
@@ -14,12 +14,12 @@ export class AlgoliaService {
 
   constructor() { }
 
-  async setOnLocalStorageFromDB(dbname: string, objectID: string, dataname: string, filter: string){
+  async setOnLocalStorageFromDB(dbname: string, dataname: string, filter: string){
     
     var initDB = searchClient.initIndex(dbname);
     
     await initDB.browse('', {
-      filters: '('+filter+':'+objectID+')'
+      filters: '('+filter+')'
     }).then(function(res){
       var data = res.hits;
       localStorage.setItem(dataname, JSON.stringify(data));
@@ -27,32 +27,54 @@ export class AlgoliaService {
   }
   async getSentReviews(userId: string){
     let data;
-    await this.setOnLocalStorageFromDB("reviews", userId, "tmpReviews", "from_id");
+    await this.setOnLocalStorageFromDB("reviews", "tmpReviews", "from_id:"+userId);
     data = JSON.parse(localStorage.getItem("tmpReviews"));
     localStorage.removeItem('tmpReviews');
     return data;
   }
   async getReceivedReviews(userId: string){
     let data;
-    await this.setOnLocalStorageFromDB("reviews", userId, "tmpReviews", "to_id");
+    await this.setOnLocalStorageFromDB("reviews", "tmpReviews", "to_id:"+userId);
     data = JSON.parse(localStorage.getItem("tmpReviews"));
     localStorage.removeItem('tmpReviews');
     return data;
   }
   async getMessageById(msgId: string){
     let data;
-    await this.setOnLocalStorageFromDB("messages", msgId, "tmpMsg", "objectID");
+    await this.setOnLocalStorageFromDB("messages", "tmpMsg", "objectID:"+msgId);
     data = JSON.parse(localStorage.getItem("tmpMsg"));
     localStorage.removeItem('tmpMsg');
     return data[0];
   }
   async getUserById(userId: string){
     let data;
-    await this.setOnLocalStorageFromDB("user-profiles", userId, "tmpUser", "objectID");
+    await this.setOnLocalStorageFromDB("user-profiles", "tmpUser", "objectID:"+userId);
     data = JSON.parse(localStorage.getItem("tmpUser"));
     localStorage.removeItem('tmpUser');
     return data[0];
   } 
+
+  async getSentMessages(userId: string){
+    let data;
+    await this.setOnLocalStorageFromDB("reviews", "tmpReviews", "from_id:"+userId+" AND senderDelete:0");
+    data = JSON.parse(localStorage.getItem("tmpReviews"));
+    localStorage.removeItem('tmpReviews');
+    return data;
+  }
+  async getReceivedMessages(userId: string){
+    let data;
+    await this.setOnLocalStorageFromDB("messages", "tmpReviews", "to_id:"+userId+" AND recipientDelete:0");
+    data = JSON.parse(localStorage.getItem("tmpReviews"));
+    localStorage.removeItem('tmpReviews');
+    return data;
+  }
+  async getArchivedMessages(userId: string){
+    let data;
+    await this.setOnLocalStorageFromDB("reviews", "tmpReviews", "to_id:"+userId+" AND archived:1");
+    data = JSON.parse(localStorage.getItem("tmpReviews"));
+    localStorage.removeItem('tmpReviews');
+    return data;
+  }
 
 
 }
